39a6d1248eb302e2a10acb9019413f6d83a66b6a
juanstn1990 <juan.paez@finkargo.com>
2026-01-16 11:56:28 -0500

    update port

--- app.py
+++ app.py
@@ -900,4 +900,4 @@ def download_video(job_id):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=80, debug=False, threaded=True)
